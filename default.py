57ddc43e973fd9848d05c84a137c87dc073ca431
b-jesch <birger.jesch@gmail.com>
2022-02-23 15:28:31 +0100

    Matrix branch created

--- default.py
+++ default.py
@@ -1,19 +1,19 @@
-# Qlock screensaver add-on by phil65
-# credits to donabi & amet
+# Qlock screensaver add-on by phil65, adapted to Matrix by PvD
+# credits to donabi, amet & phil65
 
 import os
 import sys
 import xbmcaddon
-import xbmc
+import xbmcvfs
 
 __addon__ = xbmcaddon.Addon()
 __addonid__ = __addon__.getAddonInfo('id')
-__cwd__ = __addon__.getAddonInfo('path').decode("utf-8")
-__resource__ = xbmc.translatePath(os.path.join(__cwd__, 'resources', 'lib').encode("utf-8")).decode("utf-8")
+__cwd__ = __addon__.getAddonInfo('path')
+__resource__ = xbmcvfs.translatePath(os.path.join(__cwd__, 'resources', 'lib'))
 
 sys.path.append(__resource__)
 
 if __name__ == '__main__':
-    import gui
-    screensaver_gui = gui.Screensaver('script-python-qlock.xml', __cwd__, 'default')
+    from resources.lib.gui import Screensaver
+    screensaver_gui = Screensaver('script-python-qlock.xml', __cwd__, 'default')
     screensaver_gui.doModal()
--- resources/lib/gui.py
+++ resources/lib/gui.py
@@ -1,17 +1,5 @@
-import sys
 import xbmc
 import xbmcgui
-
-__addon__ = sys.modules["__main__"].__addon__
-__addonid__ = sys.modules["__main__"].__addonid__
-__cwd__ = sys.modules["__main__"].__cwd__
-
-
-def log(txt):
-    if isinstance(txt, str):
-        txt = txt.decode("utf-8")
-    message = u'%s: %s' % (__addonid__, txt)
-    xbmc.log(msg=message.encode("utf-8"), level=xbmc.LOGDEBUG)
 
 
 class Screensaver(xbmcgui.WindowXMLDialog):
@@ -21,31 +9,12 @@ class Screensaver(xbmcgui.WindowXMLDialog):
         self.Monitor = MyMonitor(action=self.exit)
 
     def onInit(self):
-        # get addon settings
-        self.winid   = xbmcgui.Window(xbmcgui.getCurrentWindowDialogId())
-        self._get_settings()
-        self._set_prop('path', self.slideshow_path)
-        log('qlock image path: %s' % self.slideshow_path)
-
-        while (not xbmc.abortRequested) and (not self.stop):
+        while (not self.Monitor.abortRequested()) and (not self.stop):
             xbmc.sleep(1000)
 
     def exit(self):
         self.stop = True
-        # clear our properties on exit
-        self._clear_prop('path')
         self.close()
-
-    def _get_settings(self):
-        # read addon settings
-        self.slideshow_path   = __addon__.getSetting('path')
-
-    def _set_prop(self, name, value):
-        self.winid.setProperty('Qlock.%s' % name, value)
-
-    def _clear_prop(self, name):
-        self.winid.clearProperty('Qlock.%s' % name)
-
 
 
 class MyMonitor(xbmc.Monitor):
