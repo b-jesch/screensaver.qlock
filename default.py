843aa1ac005a984d59d2655993c6a31717f954bd
b-jesch <birger.jesch@gmail.com>
2022-03-28 14:52:21 +0200

    Add highlight colors and background luminance

--- default.py
+++ default.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+import xbmcgui
 import xbmcaddon
 import xbmcvfs
 
@@ -12,8 +13,18 @@ __cwd__ = __addon__.getAddonInfo('path')
 __resource__ = xbmcvfs.translatePath(os.path.join(__cwd__, 'resources', 'lib'))
 
 sys.path.append(__resource__)
+win = xbmcgui.Window(10000)
+luminance = ['cc000000', 'cc111111', 'cc222222', 'cc333333', 'cc444444', 'cc555555', 'cc666666', 'cc777777', 'cc888888']
 
 if __name__ == '__main__':
+
+    win.setProperty('Qlock.luminance', luminance[int(__addon__.getSetting('luminance'))])
+    if win.getProperty('Qlock.colorpicker'):
+        __addon__.setSetting('color', win.getProperty('Qlock.colorpicker'))
+        win.clearProperty('Qlock.colorpicker')
+
+    win.setProperty('Qlock.color', __addon__.getSetting('color'))
+
     from resources.lib.gui import Screensaver
     screensaver_gui = Screensaver('script-python-qlock.xml', __cwd__, 'default')
     screensaver_gui.doModal()
